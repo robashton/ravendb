7b07c13e45fee680737c254e74cc1d84002ba6f0
Rob Ashton <robashton@codeofrob.com>
2013-02-17 16:59:54 +0200

    Added the first leveldb artifacts

---
+++ Raven.Database/Storage/LevelDB/DocumentStorageActions.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Raven.Abstractions.Data;
+using Raven.Json.Linq;
+
+namespace Raven.Database.Storage.LevelDB
+{
+    public class DocumentStorageActions : IDocumentStorageActions
+    {
+        public IEnumerable<JsonDocument> GetDocumentsByReverseUpdateOrder(int start, int take)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<JsonDocument> GetDocumentsAfter(Etag etag, int take, long? maxSize = null, Etag untilEtag = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<JsonDocument> GetDocumentsWithIdStartingWith(string idPrefix, int start, int take)
+        {
+            throw new NotImplementedException();
+        }
+
+        public long GetDocumentsCount()
+        {
+            throw new NotImplementedException();
+        }
+
+        public JsonDocument DocumentByKey(string key, TransactionInformation transactionInformation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public JsonDocumentMetadata DocumentMetadataByKey(string key, TransactionInformation transactionInformation)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteDocument(string key, Etag etag, out RavenJObject metadata, out Etag deletedETag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AddDocumentResult AddDocument(string key, Etag etag, RavenJObject data, RavenJObject metadata)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AddDocumentResult PutDocumentMetadata(string key, RavenJObject metadata)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void IncrementDocumentCount(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AddDocumentResult InsertDocument(string key, RavenJObject data, RavenJObject metadata, bool checkForUpdates)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TouchDocument(string key, out Etag preTouchEtag, out Etag afterTouchEtag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Etag GetBestNextDocumentEtag(Etag etag)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Raven.Database/Storage/LevelDB/TransactionalLevelDb.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Raven.Database.Storage.LevelDB
+{
+    /// <summary>
+    /// Transactional wrapper on top of LevelDB
+    /// </summary>
+    public class TransactionalLevelDb
+    {
+
+    }
+}
